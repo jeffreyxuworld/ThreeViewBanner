ab08a5b63d6f4c891ff5f4d8f029001531089ffa
Lei Xu <xulei05@dobest.com>
2020-12-03 14:40:14 +0800

    修改README.md;

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # ThreeViewBanner
 [![Platform Android](https://img.shields.io/badge/platform-Android-brightgreen)](https://developer.android.com/)
 
-**一屏三页 banner 控件，并且带指示器。**
+**一屏三页 banner 控件，基于 ViewPager2 实现，并且带指示器。**
+
 **banner 后面的背景图片可随着 banner 页面的滑动，背景图在临界点会变化图片以及有 alpha 值的渐变过程。**
+
+**代码基于 Kotlin 语言编写**
 
 ThreeViewBanner:一屏三页 banner 的主要实现逻辑；
 
@@ -10,7 +13,7 @@ BaseBanner：基础 banner 控件代码；
 
 IndicatorView：banner 对应的指示器；
 
-ScaleInTransformer：通过 CompositePageTransformer 为 ViewPager2 设置一个页面缩放的ScaleInTransformer
+ScaleInTransformer：通过 CompositePageTransformer 为 ViewPager2 设置一个页面缩放的 ScaleInTransformer
 
 
 **页面运行截图：**
--- app/src/main/java/com/worldtech/threeviewbanner/activity/Utils.kt
+++ app/src/main/java/com/worldtech/threeviewbanner/activity/Utils.kt
@@ -1,31 +1,25 @@
-package com.worldtech.threeviewbanner.activity;
+package com.worldtech.threeviewbanner.activity
 
-import com.worldtech.threeviewbanner.R;
+import com.worldtech.threeviewbanner.R
+import java.util.*
 
-import java.util.ArrayList;
-import java.util.List;
+object Utils {
+    private val URLS = arrayOf(
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2860421298,3956393162&fm=26&gp=0.jpg",
+        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=163638141,898531478&fm=26&gp=0.jpg",
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1028426622,4209712325&fm=26&gp=0.jpg"
+    )
+    val DRAWABLES = intArrayOf(
+        R.drawable.growth_value_level_first_bg,
+        R.drawable.growth_value_level_second_bg,
+        R.drawable.growth_value_level_third_bg
+    )
 
-public class Utils {
-
-    private static final String[] URLS = {
-            "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2860421298,3956393162&fm=26&gp=0.jpg",
-            "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=163638141,898531478&fm=26&gp=0.jpg",
-            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1028426622,4209712325&fm=26&gp=0.jpg"
-    };
-
-    public static final int[] DRAWABLES = {
-            R.drawable.growth_value_level_first_bg,
-            R.drawable.growth_value_level_second_bg,
-            R.drawable.growth_value_level_third_bg
-    };
-
-    public static List<String> getData(int size) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(URLS[i]);
+    fun getData(size: Int): List<String> {
+        val list: MutableList<String> = ArrayList()
+        for (i in 0 until size) {
+            list.add(URLS[i])
         }
-        return list;
+        return list
     }
-
-
 }
